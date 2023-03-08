f45c18ad189c00ba035a73ef0261b84a436e59ea
bkit-cpp <84651107+bkit-cpp@users.noreply.github.com>
2023-03-08 23:29:14 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Pu Iu';
+const giftUrl = 'index2.html';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Pu iu dấu 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+const giftImage = 'pic2.jpg';
+const base64 = '';
+const giftImageBase64 = "pic3.png " + base64;
